591c447cf7e5e61cebbde876ef7332678fe40f10
Afaren <550047450@qq.com>
2017-05-01 12:01:03 +0800

    afaren - save jobs in backup dir

--- backup.js
+++ backup.js
@@ -1,13 +1,16 @@
 const agent = require('superagent');
 const async = require('async');
 const shelljs = require('shelljs');
+
 const jenkins = process.env.jenkins;
+
+const backup_dir = 'jenkins_jobs_config_backup';
 
 
 function backupJobConfig( job ) {
-	console.log(`backup config of job [ ${job.name} ] `);
-	shelljs.exec(`mkdir ${job.name}`, {silent: true});
-	shelljs.exec(`curl ${jenkins}/job/${job.name}/config.xml > ${job.name}/config.xml`, {silent: true});
+	console.log(`backup config of job --> ${job.name} `);
+	shelljs.exec(`mkdir -p ${backup_dir}/${job.name}`, {silent: true});
+	shelljs.exec(`curl ${jenkins}/job/${job.name}/config.xml > ${backup_dir}/${job.name}/config.xml`, {silent: true});
 }
 
 function done(err) {
